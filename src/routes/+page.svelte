64d69c4a66e9cec6dc20145cdbe04646f663f180
callhestia <dawidfranciszek914@gmail.com>
2026-02-03 21:20:01 +0100

    Poprawki wielkosci liter w imporcie Services

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
   
   import Hero from '$lib/components/Hero.svelte';
   import About from '$lib/components/About.svelte';
-  import Services from '$lib/components/services.svelte';
+  import Services from '$lib/components/Services.svelte';
   import Contact from '$lib/components/Contact.svelte';
 
   onMount(() => {
